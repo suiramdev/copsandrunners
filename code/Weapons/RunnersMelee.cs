ecc9514511d150c4475ca8ee0703453ef44de852
HamsterOh <mariusnouchet@gmail.com>
2022-08-05 03:33:01 +0200

    :art:  Inverted IF and added a check on runners melee

--- code/Weapons/RunnersMelee.cs
+++ code/Weapons/RunnersMelee.cs
@@ -9,26 +9,26 @@ public class RunnersMelee : Melee
 	public override void AttackPrimary()
 	{
 		base.AttackPrimary();
-		
-		if ( TraceResult.Entity != null )
+
+		if ( !TraceResult.Hit || TraceResult.Entity == null )
+			return;
+
+		var targetPawn = (Player)TraceResult.Entity;
+		if ( IsServer )
 		{
-			var targetPawn = (Player)TraceResult.Entity;
-			if ( IsServer )
-			{
-				if ( targetPawn.Role == Roles.Cop )
-					targetPawn.Knock();
+			if ( targetPawn.Role == Roles.Cop )
+				targetPawn.Knock();
 
-				TraceResult.Entity.Velocity += TraceResult.Direction * Asset.Damages * ForceMultiplier;
-			}
+			TraceResult.Entity.Velocity += TraceResult.Direction * Asset.Damages * ForceMultiplier;
+		}
 
-			if ( targetPawn.IsArrested )
-			{
-				targetPawn.Arrest( false );
+		if ( targetPawn.IsArrested )
+		{
+			targetPawn.Arrest( false );
 				
-				var particles = Particles.Create( "particles/confetti.vpcf" );
-				particles.SetEntityBone( 0, TraceResult.Entity, 2 );
-				Sound.FromWorld( "horn", TraceResult.Entity.Position );
-			}
+			var particles = Particles.Create( "particles/confetti.vpcf" );
+			particles.SetEntityBone( 0, TraceResult.Entity, 2 );
+			Sound.FromWorld( "horn", TraceResult.Entity.Position );
 		}
 	}
 }
